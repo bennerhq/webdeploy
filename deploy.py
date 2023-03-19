3c8fde9ea6920a92f61b023008244256c2c24563
Jens Kaas Benner <jens@bennerhq.com>
2023-03-19 17:43:46 +0100

    Update deploy.py
    Config file extended and minifying af JavaScript and CSS added by cli commands

--- deploy.py
+++ deploy.py
@@ -12,47 +12,85 @@
 # It's not a "bullet proff" solution, but for simple "web application"
 # it's okay!
 #
+# This version is inspired / extended from this stackoverflow post
+# https://stackoverflow.com/questions/44646481/merging-js-css-html-into-single-html
+#
 # /benner, 2023
 # jens@bennerhq.com
 #
 
 import os
+import subprocess
 import sys
 import base64
 from datetime import datetime
 from pathlib import Path
-from bs4 import BeautifulSoup
 import configparser
+from bs4 import BeautifulSoup
+import htmlmin
+
+class bcolors:
+    HEADER      = '\033[95m'
+    OKBLUE      = '\033[94m'
+    OKCYAN      = '\033[96m'
+    OKGREEN     = '\033[92m'
+    WARNING     = '\033[93m'
+    YELLOW      = '\033[93m'
+    MAGENTA     = '\033[95m'
+    FAIL        = '\033[91m'
+    ENDC        = '\033[0m'
+    BOLD        = '\033[1m'
+    UNDERLINE   = '\033[4m'
 
 def read_file(filename, utf8 = True, res = None):
     try:
         if utf8:
-            file_content = Path(filename).read_text(encoding="utf-8")
+            content = Path(filename).read_text(encoding="utf-8")
         else:
-            file_content = Path(filename).read_bytes()
+            content = Path(filename).read_bytes()
     except OSError:
-        if res == None:
-            sys.exit("Ups, can't read " + filename)
+        if res != None:
+            content = res
         else:
-            file_content = res
+            sys.exit("Ups, can't read " + filename)
 
-    return file_content
+    return content
+
+def minify(cli, content):
+    if minificss_cli:
+        result = subprocess.run(cli.split(), 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE, 
+                                text=True, 
+                                input=content)
+        if result.stderr != "":
+            sys.exit("Minify failed " + result.stdout)
+
+        content = result.stdout
+
+    return content
+
 
 # ---
 # Prepare input and output filenames
 #
 basename = os.path.basename(sys.argv[0])
 config_filename = "." + basename
+minifijs_cli = "uglifyjs"
+minificss_cli = "uglifycss"
 
-input_filename = "index.html"
-output_filename = basename + ".index.html"
 if len(sys.argv) > 1:
     input_filename = sys.argv[1]
+else:
+    input_filename = "index.html"
+
 if len(sys.argv) > 2:
     output_filename = sys.argv[2]
+else:
+    output_filename = basename + ".index.html"
 
 # ---
-# Handle confile file and build number
+# Handle config file and build number
 #
 now = datetime.now()
 now_string = now.strftime("%Y/%m/%d %H:%M:%S")
@@ -60,6 +98,11 @@ now_string = now.strftime("%Y/%m/%d %H:%M:%S")
 try:
     config = configparser.ConfigParser()
     config.read(config_filename)
+except Exception as e:
+    pass
+
+try:
+    minifijs_cli = config["minify"]["js_cli"]
 except Exception as e:
     pass
 
@@ -75,10 +118,11 @@ except Exception as e:
 
 try:
     build_no = config["build"]["number"]
-    build_no = str(int(build_no) + 1)
 except Exception as e:
     build_no = "0"
     config["build"] = {}
+
+build_no = str(int(build_no) + 1)
 
 config["build"]["number"] = build_no
 config["build"]["time_stamp"] = now_string
@@ -117,7 +161,16 @@ for tag in soup.find_all('script'):
     tag.extract()
 
 # insert scripts if they exists
-scripts += "\n\n/* AUTO GENERATED */\nDEPOLY_VERSION = true;\nDEPOLY_BUILD_NO = " + build_no + ";\nDEPLOY_TIME_STAMP = '" + now_string + "';\n\n";
+scripts += (
+    "\n\n" 
+    "/* AUTO GENERATED */\n"
+    "DEPOLY_VERSION = true;\n"
+    "DEPOLY_BUILD_NO = " + build_no + ";\n"
+    "DEPLOY_TIME_STAMP = '" + now_string + "';\n"
+    "\n"
+)
+
+scripts = minify(minifijs_cli, scripts)
 
 new_script = soup.new_tag('script')
 new_script.string = scripts
@@ -140,6 +193,8 @@ for tag in soup.find_all('link', rel="stylesheet", href=True):
     tag.extract()
 
 if len(styles) != 0:
+    styles = minify(minificss_cli, styles)
+
     # insert styles if they exists
     new_style = soup.new_tag('style')
     new_style.string = styles
@@ -170,9 +225,12 @@ for tag in soup.find_all('img', src=True):
 # ---
 # Save onto a single html file
 #
-print("> " + output_filename + ' | ' + now_string + ", build #" + build_no)
+print("> " + output_filename + ' | ' + now_string + ", build " + bcolors.BOLD + bcolors.YELLOW + "#" + build_no + bcolors.ENDC)
+
+final_html = str(soup)
+final_html = htmlmin.minify(final_html)
 
 try:
-    Path(output_filename).write_text(str(soup), encoding="utf-8")
+    Path(output_filename).write_text(final_html, encoding="utf-8")
 except OSError:
     sys.exit("Ups, can't write " + output_filename)
